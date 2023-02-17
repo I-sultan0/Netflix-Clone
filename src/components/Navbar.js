d81f954fe7db2e784eb61be0dcd53db01b99b284
Sultan <akhtersultan007@gmail.com>
2023-02-17 17:30:20 +0530

    Added Trailer Popups

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Navbar.module.css";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [show, handleShow] = useState(false);
@@ -19,11 +20,7 @@ const Navbar = () => {
 
   return (
     <div className={[classes.navbar, show && classes.navbar_black].join(" ")}>
-      <img
-        className={classes.navbar_logo}
-        src="https://fontmeme.com/temporary/74fd7f5b481c0de8ba2ac65e96541ae2.png"
-        alt="Netflix Logo"
-      />
+      <img className={classes.navbar_logo} src={Logo} alt="Netflix Logo" />
       <img
         className={classes.navbar_avatar}
         src="https://imgs.search.brave.com/FG75L8fOc2sNGc7Uf1tl9CrwqytX_DZn5HfQhi3bi6Q/rs:fit:320:320:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2UzLzk0/LzMwL2UzOTQzMDQz/NGQyYjgyMDcxODhm/ODgwYWM2NmM2NDEx/LnBuZw"
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import classes from "./Row.module.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchURL, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
@@ -16,7 +20,30 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
     fetchData();
   }, [fetchURL]);
 
-  // console.table(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  console.table(movies);
 
   return (
     <div className={classes.row}>
@@ -25,6 +52,7 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={[
               classes.row_poster,
               isLargeRow && classes.row_posterLarge,
@@ -36,6 +64,7 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}{" "}
     </div>
   );
 };
